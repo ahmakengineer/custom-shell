8d8b58abe12863e7e1e51f6f324100178241d16a
Shahid <royalshahid36@gmail.com>
2025-12-22 17:38:00 +0530

    Implementing exit

--- src/main.c
+++ src/main.c
@@ -13,6 +13,10 @@ int main(int argc, char *argv[]) {
       char command[1024];
       fgets(command, sizeof(command), stdin);
       command[strcspn(command, "\n")] = '\0';
+
+      if(strcmp(command, "exit") == 0) {
+          break;
+      }
       printf("%s: command not found\n", command);
   }
 
